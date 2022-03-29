4e6b5fe8a36e200ba08bda8ee63b841468c66dff
Yang Cheng <annacheng09@hotmail.com>
2022-03-28 18:47:41 -0700

    fix deploying

--- server.js
+++ server.js
@@ -23,11 +23,13 @@ app.use(express.static('public'));
 
 // app.get
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 app.get('/*', (req, res) => 
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // app.listen
-app.listen(PORT)
+app.listen(PORT, () =>
+  console.log(`App listening 🚀`)
+);
